23a5d39fc39241b909bd15455c897d7fad545d06
黎宝芝 <baozhili2012@live.hk>
2017-06-11 11:22:33 +0800

    【新增】导入包和源码

---
+++ src/StyleMgr.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace ProjectPro {
+    static class StyleMgr {
+        public static Texture2D ListUp;
+        public static Texture2D ListDown;
+        public static Texture2D PreUp;
+        public static Texture2D PreDown;
+        public static Texture2D NextUp;
+        public static Texture2D NextDown;
+        public static Texture2D UpUp;
+        public static Texture2D UpDown;
+
+        static StyleMgr() {
+            ListUp = EditorGUIUtility.Load("Assets/ProjectPro/list_up.png") as Texture2D;
+            ListDown = EditorGUIUtility.Load("Assets/ProjectPro/list_down.png") as Texture2D;
+            PreUp = EditorGUIUtility.Load("Assets/ProjectPro/pre_up.png") as Texture2D;
+            PreDown = EditorGUIUtility.Load("Assets/ProjectPro/pre_down.png") as Texture2D;
+            NextUp = EditorGUIUtility.Load("Assets/ProjectPro/next_up.png") as Texture2D;
+            NextDown = EditorGUIUtility.Load("Assets/ProjectPro/next_down.png") as Texture2D;
+            UpUp = EditorGUIUtility.Load("Assets/ProjectPro/up_up.png") as Texture2D;
+            UpDown = EditorGUIUtility.Load("Assets/ProjectPro/up_down.png") as Texture2D;
+        }
+    }
+}
